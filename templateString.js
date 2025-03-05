d403aa7bc6e7c01d55eb3e7d98d65707e0034706
mbiplobmollah <biplobmollah16@gmail.com>
2025-03-05 14:37:52 +0600

    learn about functions and string literals

---
+++ templateString.js
@@ -0,0 +1,19 @@
+const anthem = `
+fdrefdfda dfsafdsf dsf df
+sdffdg dsffdfdf
+dfdfsdfds
+`;
+
+console.log(anthem)
+
+const countryName = 'Bangladesh';
+
+const text = 'amar desh ' + countryName+ ' eikhane nodi ache onek';
+
+console.log(text);
+
+
+const text1 = `amar desh ${countryName} eikhane nodi ache onek`
+
+console.log(text1)
+
