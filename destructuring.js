4289fd27bd0b03556c8daa79fbe3c4616a6bf430
mbiplobmollah <biplobmollah16@gmail.com>
2025-03-05 16:59:23 +0600

    learn about destructuring

---
+++ destructuring.js
@@ -0,0 +1,21 @@
+const hena = {
+    name: 'hena',
+    age: 18,
+    friend: ['hero alom', 'bappa'],
+    status: 'not found'
+}
+// const henaStatus = hena.status;
+// const henaAge = hena.age;
+// const henaName = hena.name;
+// console.log(henaStatus);
+
+
+// const {name,status} = hena;
+
+// console.log(status);
+
+
+const person =['hero alom',{name: 'biplob'}, 'hena', 'bapparaj'];
+
+const [nayok,myname] = person;
+console.log(nayok, myname)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,16 @@
+const hena = {
+    name: 'hena',
+    age: 18,
+    friend: ['hero alom', 'bappa'],
+    status: 'not found'
+}
+// const henaStatus = hena.status;
+// const henaAge = hena.age;
+// const henaName = hena.name;
+// console.log(henaStatus);
+
+
+const {name,age,status,friend} = hena;
+
+
+console.log(age);
