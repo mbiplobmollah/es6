286699b0c032e6a2b0447a004b1e27cbb4ce0cf9
mbiplobmollah <biplobmollah16@gmail.com>
2025-03-05 16:46:22 +0600

    learn about spread operator

---
+++ spreadoperator.js
@@ -0,0 +1,9 @@
+// const friends = ['sokina', 'jorina','hena'];
+
+// const newArray = ['hero alom', ...friends];
+// console.log(newArray);
+
+
+const numbers = [1,3,4,5,6,7,8,9,34,56,78,89];
+
+console.log(Math.max(...numbers));
